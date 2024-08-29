f8ee6619596bb5b1e73a7d890c72029e2f3301da
Rodrigo Limões <rolimoes@gmail.com>
2024-08-29 09:10:19 -0300

    feat: add validate middleware

---
+++ src/middleware/validate.ts
@@ -0,0 +1,50 @@
+import { ClassConstructor, plainToInstance } from "class-transformer";
+import { NextFunction } from "express";
+import { validateOrReject, ValidationError } from "class-validator";
+import { HttpException } from "@src/common/utils/httpException";
+import { StatusCodes } from "@src/common/enums/statusCodes";
+
+const getMessagesError = (err: Array<ValidationError>) => {
+  let messages = [];
+
+  for (let i = 0; i < err.length; i++) {
+    const constraints = err[i]["constraints"];
+
+    for (const key in constraints) {
+      if (constraints) {
+        messages.push(constraints[key]);
+      }
+    }
+  }
+
+  return messages;
+};
+
+type Target = "body" | "query" | "params";
+
+export const validate =
+  (target: Target = "body") =>
+  (objDTO: ClassConstructor<any>) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      if (req[target]) {
+        const dto = plainToInstance(objDTO, req[target]);
+        await validateOrReject(dto);
+
+        req[target] = dto;
+
+        next();
+      } else {
+        next(new Error("Target validation is not valid"));
+      }
+    } catch (e) {
+      console.log(e);
+      next(
+        new HttpException({
+          statusCode: StatusCodes.BAD_REQUEST,
+          message: getMessagesError(e)[0],
+          errorCode: "INVALID_DATA",
+        })
+      );
+    }
+  };
