98a669b4928388035c07d2ad9b92febcd200a708
Rodrigo Limões <rolimoes@gmail.com>
2024-08-30 23:17:36 -0300

    chore: add healthcheck

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,6 @@ FROM node:18.20.4-alpine as build
 
 WORKDIR /app
 
-RUN mkdir -p /app/tmp/uploads
-
 COPY package*.json ./
 RUN npm install
 
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import express, { Express } from "express";
-import { appMiddleware } from "@src/middleware/app";
-import { errorHandler } from "@src/middleware/errorHandler";
-import { router } from "@src/routes";
-import { connectDatabase } from "@src/database";
-import "reflect-metadata";
+import express, { Express } from 'express';
+import { appMiddleware } from '@src/middleware/app';
+import { errorHandler } from '@src/middleware/errorHandler';
+import { router } from '@src/routes';
+import { connectDatabase } from '@src/database';
+import 'reflect-metadata';
 
 const app: Express = express();
 const port = 80;
@@ -13,7 +13,8 @@ appMiddleware(app);
 app.use(router);
 app.use(errorHandler);
 
-app.listen(port, async () => {
-  console.log(`Server is running at http://localhost:${port}`);
-  await connectDatabase();
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}`);
 });
+
+connectDatabase();
