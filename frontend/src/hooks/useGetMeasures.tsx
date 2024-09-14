ad114361afd55e7614c1266cafec5320c60d1d46
Rodrigo Limões <rolimoes@gmail.com>
2024-09-14 17:13:15 -0300

    feat: add filter in measure list

--- frontend/src/hooks/useGetMeasures.tsx
+++ frontend/src/hooks/useGetMeasures.tsx
@@ -1,17 +1,22 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
 
-export const useGetMeasures = (id) => {
+export const useGetMeasures = ({ id, type }) => {
   const [data, setData] = useState<any>();
 
   useEffect(() => {
-    fetch(`http://localhost:80/${id}/list`, { method: "GET" }).then(
-      async (r) => {
-        const response = await r.json();
-        setData(response);
-      }
-    );
-  }, [id]);
+    const url =
+      type === "all"
+        ? `http://localhost:80/${id}/list`
+        : `http://localhost:80/${id}/list?measure_type=${type}`;
+
+    fetch(url, {
+      method: "GET",
+    }).then(async (r) => {
+      const response = await r.json();
+      setData(response);
+    });
+  }, [id, type]);
 
   return {
     data,
--- frontend/src/pages/ViewMeasures/ViewMeasures.tsx
+++ frontend/src/pages/ViewMeasures/ViewMeasures.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Table } from "@/components/Table";
 import { useGetMeasures } from "@/hooks/useGetMeasures";
@@ -10,28 +10,46 @@ type ViewMeasuresProps = ViewMeasuresStateProps & ViewMeasuresDispatchProps;
 
 const ViewMeasures: FC<ViewMeasuresProps> = () => {
   const { id } = useParams();
-  const { data } = useGetMeasures(id);
+  const [type, setType] = useState("all");
+  const { data } = useGetMeasures({ id, type });
 
   return (
-    <>
-      <Table
-        columns={[
-          {
-            id: "datetime",
-            label: "Data da medição",
-          },
-          {
-            id: "type",
-            label: "Tipo",
-          },
-        ]}
-        items={{
-          datetime: ({ measure_datetime }) => measure_datetime,
-          type: ({ measure_type }) => measure_type,
-        }}
-        data={data?.measures || []}
-      />
-    </>
+    <div className="flex gap-6 flex-col">
+      <div className="filter-container">
+        <label htmlFor="filter">Filtrar por:</label>
+        <select
+          id="filter"
+          className="filter-dropdown"
+          value={type}
+          onChange={(event) => {
+            setType(event.target.value);
+          }}
+        >
+          <option value="all">Todos</option>
+          <option value="WATER">Água</option>
+          <option value="GAS">Gás</option>
+        </select>
+      </div>
+      <div>
+        <Table
+          columns={[
+            {
+              id: "datetime",
+              label: "Data da medição",
+            },
+            {
+              id: "type",
+              label: "Tipo",
+            },
+          ]}
+          items={{
+            datetime: ({ measure_datetime }) => measure_datetime,
+            type: ({ measure_type }) => measure_type,
+          }}
+          data={data?.measures || []}
+        />
+      </div>
+    </div>
   );
 };
 
